ccfd95a326be81ce42d39195a46f95a5fa890a77
piglovesyou <thepiglovesyou@gmail.com>
2019-06-08 10:59:02 +0900

    m

--- packages/uwf/src/data/schema.ts
+++ packages/uwf/src/data/schema.ts
@@ -13,44 +13,9 @@ import merge from 'lodash.merge';
 import schemaDeps from '../../__generated__/schemaDeps';
 import graphqlDeps from '../../__generated__/graphqlDeps';
 
-// import {
-//   schema as NewsSchema,
-//   resolvers as NewsResolvers,
-//   queries as NewsQueries,
-// } from './graphql/News/schema';
-//
-// import {
-//   schema as OnMemoryStateSchema,
-//   queries as OnMemoryStateQueries,
-//   mutations as OnMemoryStateMutations,
-// } from './graphql/OnMemoryState/schema';
-
-// import {
-//   schema as DatabaseSchema,
-//   resolvers as DatabaseResolvers,
-//   mutations as DatabaseMutations,
-//   queries as DatabaseQueries,
-// } from './graphql/Database/schema';
-
-// import {
-//   schema as TimestampSchema,
-//   resolvers as TimestampResolvers,
-// } from './graphql/Scalar/Timestamp';
-
 const RootQuery =  [
   `
-  
-  # # React-Starter-Kit Querying API
-  # ### This GraphQL schema was built with [Apollo GraphQL-Tools](https://github.com/apollographql/graphql-tools)
-  # _Build, mock, and stitch a GraphQL schema using the schema language_
-  #
-  # **[Schema Language Cheet Sheet](https://raw.githubusercontent.com/sogko/graphql-shorthand-notation-cheat-sheet/master/graphql-shorthand-notation-cheat-sheet.png)**
-  #
-  # 1. Use the GraphQL schema language to [generate a schema](https://www.apollographql.com/docs/graphql-tools/generate-schema.html) with full support for resolvers, interfaces, unions, and custom scalars. The schema produced is completely compatible with [GraphQL.js](https://github.com/graphql/graphql-js).
-  # 2. [Mock your GraphQL API](https://www.apollographql.com/docs/graphql-tools/mocking.html) with fine-grained per-type mocking
-  # 3. Automatically [stitch multiple schemas together](https://www.apollographql.com/docs/graphql-tools/schema-stitching.html) into one larger API
   type RootQuery {
-    
     ${schemaDeps.map(([module, relPath]) => {
       return module.queries && module.queries.join('\n');
     }).filter(s => Boolean(s)).join('\n')}
@@ -60,15 +25,6 @@ const RootQuery =  [
 
 const Mutation =
   schemaDeps.some(([module]) => module.mutations) ? [ `
-  # # React-Starter-Kit Mutating API
-  # ### This GraphQL schema was built with [Apollo GraphQL-Tools](https://github.com/apollographql/graphql-tools)
-  # _Build, mock, and stitch a GraphQL schema using the schema language_
-  #
-  # **[Schema Language Cheet Sheet](https://raw.githubusercontent.com/sogko/graphql-shorthand-notation-cheat-sheet/master/graphql-shorthand-notation-cheat-sheet.png)**
-  #
-  # 1. Use the GraphQL schema language to [generate a schema](https://www.apollographql.com/docs/graphql-tools/generate-schema.html) with full support for resolvers, interfaces, unions, and custom scalars. The schema produced is completely compatible with [GraphQL.js](https://github.com/graphql/graphql-js).
-  # 2. [Mock your GraphQL API](https://www.apollographql.com/docs/graphql-tools/mocking.html) with fine-grained per-type mocking
-  # 3. Automatically [stitch multiple schemas together](https://www.apollographql.com/docs/graphql-tools/schema-stitching.html) into one larger API
   type Mutation {
     
     ${schemaDeps.map(([module, relPath]) => {
@@ -87,8 +43,6 @@ const SchemaDefinition = [
 `,
 ];
 
-// Merge all of the resolver objects together
-// Put schema together into one array of schema strings
 const resolvers = merge.apply(null,
   schemaDeps.map(([module, relPath]) => {
     return module.resolvers && module.resolvers;
