3fd1e53107ff0c89449d7e7409a125ecd7cc4f83
piglovesyou <thepiglovesyou@gmail.com>
2019-06-09 12:06:11 +0900

    refac

---
+++ packages/uwf/src/core/createApolloClient/clientSchema.ts
@@ -0,0 +1,7 @@
+import gql from "graphql-tag";
+import merge from 'lodash.merge';
+import schemaDeps from '../../../__generated__/clientSchemaDeps';
+
+export const clientSchema = gql(schemaDeps.map(([m]) => m.schema).join('\n'));
+export const clientDefaults = merge.apply(null, [{}, ...schemaDeps.map(([m]) => m.defaults).filter(Boolean)]);
+export const clientResolvers = merge.apply(null, [{}, ...schemaDeps.map(([m]) => m.resolvers).filter(Boolean)]);
--- packages/uwf/src/core/createApolloClient/createApolloClient.server.ts
+++ packages/uwf/src/core/createApolloClient/createApolloClient.server.ts
@@ -3,14 +3,8 @@ import { from } from 'apollo-link';
 import { onError } from 'apollo-link-error';
 import { SchemaLink } from 'apollo-link-schema';
 import merge from 'lodash.merge';
-import gql from 'graphql-tag';
+import { clientDefaults, clientResolvers, clientSchema } from "./clientSchema";
 import createCache from './createCache';
-
-import schemaDeps from '../../../__generated__/clientSchemaDeps';
-
-const clientSchema = gql(schemaDeps.map(([m]) => m.schema).join('\n'));
-const clientDefaults = merge.apply(null, [{}, ...schemaDeps.map(([m]) => m.defaults).filter(Boolean)]);
-const clientResolvers = merge.apply(null, [{}, ...schemaDeps.map(([m]) => m.resolvers).filter(Boolean)]);
 
 export default function createApolloClient(
   schema: SchemaLink.Options,
@@ -27,10 +21,10 @@ export default function createApolloClient(
       if (graphQLErrors)
         graphQLErrors.map(({ message, locations, path }) =>
           console.warn(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            `[GraphQL error]: Message: ${ message }, Location: ${ locations }, Path: ${ path }`,
           ),
         );
-      if (networkError) console.warn(`[Network error]: ${networkError}`);
+      if (networkError) console.warn(`[Network error]: ${ networkError }`);
     }),
     new SchemaLink({ ...schema }),
   ]);
