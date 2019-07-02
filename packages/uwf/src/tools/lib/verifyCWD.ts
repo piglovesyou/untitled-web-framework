e2463bb4e3f328617d820b10b254ce971dfdd991
piglovesyou <thepiglovesyou@gmail.com>
2019-07-02 22:29:14 +0900

    m

--- packages/uwf/src/tools/lib/verifyCWD.ts
+++ packages/uwf/src/tools/lib/verifyCWD.ts
@@ -1,19 +1,21 @@
 import path from 'path';
 
 export default function verifyCWD() {
+  // TODO: Not sure how to verify..
+
   // @ts-ignore
-  const pkgPath = path.resolve(process.env.INIT_CWD, 'package.json');
-  let pkg;
-  try {
-    // eslint-disable-next-line global-require
-    pkg = require(pkgPath);
-    if (pkg.devDependencies || pkg.devDependencies.uwd) {
-      return;
-    }
-  } catch (e) {
-    throw new Error(
-      'You have to run uwf command on your project root where package.json are located.',
-    );
-  }
+  // const pkgPath = path.resolve(process.env.INIT_CWD, 'package.json');
+  // let pkg;
+  // try {
+  //   // eslint-disable-next-line global-require
+  //   pkg = require(pkgPath);
+  //   if (pkg.devDependencies || pkg.devDependencies.uwd) {
+  //     return;
+  //   }
+  // } catch (e) {
+  //   throw new Error(
+  //     'You have to run uwf command on your project root where package.json are located.',
+  //   );
+  // }
   throw new Error("Your package.json doesn't contain devDependencies.uwf .");
 }
