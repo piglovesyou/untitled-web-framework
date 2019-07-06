6edb80fcd33870d2277651f63dd0616d9422086d
piglovesyou <thepiglovesyou@gmail.com>
2019-07-06 15:24:04 +0900

    m

--- prepare-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-yarn
-yarn lerna bootstrap
-yarn lerna exec yarn
-(cd examples/basic && cd `yarn bin` && ln -s ../../../../packages/uwf/bin/uwf .)
-yarn lerna run prepack
-(cd examples/basic && yarn codegen)
